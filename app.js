4d7b94abcf19eb94ff7c6880b44c7935a3243654
JairJOP <jairjosue.jo@gmail.com>
2024-11-01 15:21:26 +0000

    Pushing my app files

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+require('dotenv/config')
+
+const bodyParser = require('body-parser')
+const postsRoute = require('./routes/posts')
+
+
+app.use(bodyParser.json()) // everything coming in the application is a json
+app.use('/posts', postsRoute)
+
+app.get('/', (req, res) =>{
+    res.send('Homepage')
+})
+
+mongoose.connect(process.env.DB_CONNECTOR).then(()=>{
+    console.log('DB is now connected!')
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is up and running...')
+})
